785b6ad9d3a41337e55892cb98ca6309099fa069
ave_mrk <zhivokinaolyga@mail.ru>
2023-10-21 14:34:49 +0300

    solved task

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
+}
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe
+        src={props.url}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  );
+}
+
+function List(props) {
+  return props.list.map((item) => {
+    if (item.views > 100) {
+      switch (item.type) {
+        case "video":
+          return (
+            <Popular>
+              <Video {...item} />
+            </Popular>
+          );
+
+        case "article":
+          return (
+            <Popular>
+              <Article {...item} />
+            </Popular>
+          );
+      }
+    } else {
+      if (item.views < 100) {
+        switch (item.type) {
+          case "video":
+            return (
+              <New>
+                <Video {...item} />
+              </New>
+            );
+
+          case "article":
+            return (
+              <New>
+                <Article {...item} />
+              </New>
+            );
+        }
+      }
+    }
+  });
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 50
+    },
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 12
+    },
+    {
+      type: "article",
+      title: "Невероятные события в неизвестном поселке...",
+      views: 175
+    },
+    {
+      type: "article",
+      title: "Секретные данные были раскрыты!",
+      views: 1532
+    },
+    {
+      type: "video",
+      url:
+        "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 4253
+    },
+    {
+      type: "article",
+      title: "Кот Бегемот обладает невероятной...",
+      views: 12
+    }
+  ]);
+
+  return <List list={list} />;
+}
